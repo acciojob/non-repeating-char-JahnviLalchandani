889d33c947522efdc05124ed325fce03094fa550
Jahnvi Lalchandani <166894721+JahnviLalchandani@users.noreply.github.com>
2026-09-04 19:55:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	let char=null;
+ for(let i=0;i<str.length;i++){
+	 if(!(str[i]===str[i-1] || str[i]===str[i+1])){
+		 char=str[i]
+	 }
+ }
+	return char
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
+
