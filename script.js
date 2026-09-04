a588d8217872ed64df438454867c6b8f5ed7429c
Jahnvi Lalchandani <166894721+JahnviLalchandani@users.noreply.github.com>
2026-09-04 20:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function firstNonRepeatedChar(str) {
-	let char=null;
+ if(!str) return null;
  for(let i=0;i<str.length;i++){
-	 if(!(str[i]===str[i-1] || str[i]===str[i+1])){
-		 char=str[i]
+	let count=0;
+	 for(let j=0;j<str.length;j++){
+		 if(str[i]===str[j]){
+			 count++;
+		 }
 	 }
+		 if(count===1) return str[i];
  }
-	return char
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+
 
 
